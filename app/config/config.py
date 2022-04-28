bda9880093986e344658950af22153eea25ca57b
mchlbatista <20080764+mchlbatista@users.noreply.github.com>
2022-04-27 22:29:23 -0500

    Updated config to point to the right env var.

--- app/config/config.py
+++ app/config/config.py
@@ -4,4 +4,4 @@ from os import getenv
 class Config(object):
 
     SQLALCHEMY_DATABASE_URI = getenv("SQLALCHEMY_DATABASE_URI", "sqlite:///db.sqlite3")
-    SQLALCHEMY_TRACK_MODIFICATIONS = getenv("SQLALCHEMY_DATABASE_URI", False)
+    SQLALCHEMY_TRACK_MODIFICATIONS = getenv("SQLALCHEMY_TRACK_MODIFICATIONS", False)
